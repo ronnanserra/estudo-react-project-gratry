541d9da0189efb5d7f6930a03eacf51c50a85bc3
ronnan.serra <ronnan.serra@tivit.com>
2020-07-09 22:52:21 -0300

    Código inicial, exemplos didáticos de como transformar class em function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [showImage, setShowImage] = useState(false);
+
+  const toggle = () => {
+    setShowImage(!showImage);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button type="button" onClick={toggle}>
+            {showImage ? 'Esconder' : 'Mostrar' }
+        </button>
+          {showImage && (
+            <img src={logo} className="App-logo" alt="logo" />
+          )}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,6 +33,7 @@ function App() {
       </header>
     </div>
   );
+  
 }
 
 export default App;
